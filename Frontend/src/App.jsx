5699332f9dde981babd7d3dc134489aa70d0e425
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2025-10-29 16:48:11 +0530

    dev

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HomePage } from "./Pages/HomePage";
+import HomePage  from "./Pages/HomePage";
 import { NoteDetailsPage } from "./Pages/NoteDetailsPage";
 import { CreatePage } from "./Pages/CreatePage";
 
