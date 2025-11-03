d151bf4c95c2df4ee8e1dc4dc48c237ac70d6482
Sadeepa Dinakara <167463768+Hasalapera@users.noreply.github.com>
2025-11-03 14:14:16 +0530

    addnote frontend

--- Frontend/vite-project/src/Pages/AddNote/AddNote.jsx
+++ Frontend/vite-project/src/Pages/AddNote/AddNote.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import "./AddNote.css";
 
 const AddNote = () => {
-  return <div>AddNote</div>;
+  return (
+    <>
+      <h1 className="addnote_title">Save Your Thinks From Here</h1>
+      <div className="add_nt_form">
+        <form>
+          <table>
+            <tr>
+              <td>title:</td>
+              <td>
+                <input type="text"></input>
+              </td>
+            </tr>
+            <tr>
+              <td>Content:</td>
+              <td>
+                <textarea></textarea>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button>Add</button>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </div>
+    </>
+  );
 };
 export default AddNote;
