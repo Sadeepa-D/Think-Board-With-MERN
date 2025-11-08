b07a55df699a2d3ecf81d72d781336d497cffc1a
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2025-11-08 10:06:19 +0530

    notedetails update page done

--- Frontend/vite-project/src/Pages/NoteDetails/NoteDetails.jsx
+++ Frontend/vite-project/src/Pages/NoteDetails/NoteDetails.jsx
@@ -1,6 +1,41 @@
 import React from "react";
+import "./NoteDetails.css";
 
 export const NoteDetails = () => {
-  return <div>NoteDetails</div>;
+  return (
+    <>
+      <h1 className="updatenote_title">Update Your Think</h1>
+      <div className="update_nt_form">
+        <form>
+          <table>
+            <tbody>
+              <tr>
+                <td>Title:</td>
+                <td>
+                  <input type="text" />
+                </td>
+              </tr>
+              <tr>
+                <td>Content:</td>
+                <td>
+                  <textarea />
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2" className="button_row">
+                  <button type="submit" className="update_btn">
+                    Update
+                  </button>
+                  <button type="button" className="delete_btn">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+      </div>
+    </>
+  );
 };
 export default NoteDetails;
